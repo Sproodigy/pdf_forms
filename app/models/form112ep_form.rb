2f80da8266c7ec224ee11fe2bf81caac94816526
sproodigy <sproodigy@gmail.com>
2014-08-11 21:28:02 +0400

    Продолжена форма 112ep

--- app/models/form112ep_form.rb
+++ app/models/form112ep_form.rb
@@ -1,7 +1,7 @@
 class Form112epForm < Prawn::Document
 
 	def create_table(columns, x)
-		table [[' '] * columns], cell_style: {width: 10, height: 10}, position: x
+		table [[' '] * columns], cell_style: {width: 10, height: 10, borders: [:bottom, :left, :right]}, position: x
 	end
 
 	def print_form112ep(sender:, receiver:, sender_address:, receiver_address:, tel:, value:, payment:,
@@ -45,49 +45,46 @@ class Form112epForm < Prawn::Document
 		stroke_horizontal_line 28, 79, at: 613
 		stroke_horizontal_line 11, 79, at: 600
 		draw_text 'Контроль', at: [0, 591], rotate: 90, size: 6
-		draw_text '(дата)', at: [21, 164], size: 6
-		draw_text '(подпись)', at: [17, 158], size: 6
+		draw_text '(дата)', at: [21, 594], size: 6
+		draw_text '(подпись)', at: [17, 588], size: 6
 
 		# Зона почтового перевода
 
 		draw_text 'Обведённое жирной линией заполняется отправителем реревода или РПО', at: [0, 320], size: 6, rotate: 90
+		draw_text 'Исправления не допускаются', at: [550, 450], size: 6, rotate: 270
 
-		bounding_box([4, cursor-20], width: 540) do
-			move_down 2
-
-			indent(2) do
-				text "ПОЧТОВЫЙ ПЕРЕВОД\nна 38908 руб  00 коп"
-			end
-
-			# fill_color 'dddddd'
-			# fill_rectangle [130, 20], 355, 10
-			# fill_color '000000'
-
-			bounding_box([2, cursor], width: 100, height: 50) do
-				text "Наложенный платёж", align: :center, valign: :center
-				stroke_bounds
-			end
-			move_down 2
-
-			indent(2) do
-				formatted_text [{text: 'Кому:', styles: [:bold]}, {text: '_' * 100}]
-
-			end
-			move_down 20
-
-			draw_text 'Рас/счёт: ' + ' ' * 90 + 'БИК: ', at: [2, cursor]
-			move_cursor_to 8
-			create_table(20, 52)
-			move_cursor_to 10
-			create_table(9, 420)
-
-
-
-			stroke_bounds
+		stroke do
+			rectangle [3, 580], 544, 247
+			rectangle [3, 330], 544, 247
 		end
 
+
+		# bounding_box([4, cursor-300], width: 533) do
+		# 		formatted_text_box [{text: "ИНН:" + ' ' * 68 + 'Кор/счёт:', styles: [:bold]},
+		# 		                    {text: "\nНаименование банка: Поволжский филиал 'Альфа-Банк' в Самаре", styles: [:bold]},
+		# 		                    {text: "\nРас/счёт:" + ' ' * 101 + 'БИК:', styles: [:bold]}],
+		# 		                   at: [2, cursor-4], leading: 3
+		# 		move_up cursor-3
+		# 		create_table(12, 35)
+		# 		move_up cursor+10
+		# 		create_table(20, 328)
+		# 		move_up cursor-20
+		# 		create_table(20, 59)
+		# 		move_up cursor+10
+		# 		create_table(9, 438)
+		#
+		#
+		#
+		# 		stroke_bounds
+		# 	end
+
+
+
+
+
+
 		text_box 'Являетесь ли Вы должностным лицом публичных международных организаций или лицом, замещающим (занимающим) гос. должности РФ, должности членов Совета директоров Центрального банка РФ, должности федеральной гос. службы, назначение на которые и освобождение от которых осуществляется Президентом РФ или Правительством РФ, должности в Центральном банке РФ, гос. компаниях и иных организациях, созданных РФ на основании Федеральных законов, включенных в перечни должностей, определяемые Президентом РФ? (на основании федерального закона №231-ФЗ от 18 декабря 2006 г.)',
-		         at: [9, 130], width: 500, size: 6
+		         at: [9, 80], width: 500, size: 6
 		text_box "В целях осуществления данного почтового перевода подтверждаю свое согласие:\n- на обработку как автоматизированным, так и неавтоматизированным способом указанных на бланке персональных данных; (Закон №152-ФЗ от 14 июля 2006 г.)\n- на передачу информации о номере почтового перевода, о событии (о перечислении
  почтового перевода на счет получателя, о дате и месте совершения события).",
 		         at: [9, 17.5], width: 500, size: 6
